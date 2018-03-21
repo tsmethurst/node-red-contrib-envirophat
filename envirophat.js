e937f6d8cf7c83a88129fac7155eadb348b3fee5
Tobi Smethurst <tobi.smethurst@klarrio.com>
2018-03-21 12:29:33 +0100

    Version 1.0.0. First working commit.

--- envirophat.js
+++ envirophat.js
@@ -1,20 +1,25 @@
 module.exports = function (RED) {
-    const PythonShell = require('python-shell');
+    var PythonShell = require('python-shell');
 
     function EnvirophatNode(config) {
         RED.nodes.createNode(this, config);
 
+        var node = this
 
-        var pyshell = new PythonShell('script.py');
+        var pyshell = new PythonShell('script.py', {scriptPath: __dirname});
 
         pyshell.on('message', function (messageString) {
-            var msg
-            const envirophat = JSON.parse(messageString.replace(/'/g, '"'));
-            msg.envirophat = envirophat
-            this.send(msg)
+            var results = JSON.parse(messageString.replace(/'/g, '"'));
+            var msg = {
+                payload: "",
+                envirophat: results
+            }
+            node.send(msg)
         });
 
-
+        pyshell.on('error', error => {
+            node.error(error)
+        })
     }
 
     RED.nodes.registerType("envirophat", EnvirophatNode);
